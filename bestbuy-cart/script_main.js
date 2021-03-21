5ed9dbe764415271e8ca5a14c5549a1d47e8e545
albert-sun <albert.h.sun@gmail.com>
2021-03-21 01:08:44 -0400

    autoreload changes, you DON'T wanna change it to 0...

--- bestbuy-cart/script_main.js
+++ bestbuy-cart/script_main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Best Buy Automation (Cart Saved Items)
 // @namespace    akito
-// @version      2.5.0
+// @version      2.5.1
 // @description  Best Buy queue automation for saved items from the cart page
 // @author       akito#9528 / Albert Sun
 // @updateURL    https://raw.githubusercontent.com/albert-sun/tamper-scripts/main/bestbuy-cart/script_main.js
@@ -24,7 +24,7 @@
 /* globals generateInterface, generateWindow, designateLogging, designateSettings */
 const j$ = $; // Just in case websites like replacing $ with some abomination
 
-const version = "2.5.0";
+const version = "2.5.1";
 const scriptName = "bestBuy-cartSavedItems"; // Key prefix for settings retrieval
 const scriptText = `Best Buy (Cart Saved Items) v${version} | Albert Sun / akito#9528`;
 const messageText = "Thank you and good luck! | https://github.com/albert-sun/tamper-scripts";
@@ -36,7 +36,7 @@ const settings = {
     initialClick: { description: "Auto-Add Button Clicking", type: "boolean", value: true },
     colorInterval: { description: "Color Polling Interval (ms)", type: "number", value: 250 },
     loadUnloadInterval: { description: "Load/Unload Polling Interval (ms)", type: "number", value: 50 },
-    autoReloadInterval: { description: "Auto Page Reload Interval (ms, 0 to disable)", type: "number", value: 0},
+    autoReloadInterval: { description: "Auto Page Reload Interval (ms, <1000 to disable)", type: "number", value: 0},
     errorResetDelay: { description: "Error Reset Delay (ms)", type: "number", value: 250 },
     cartCheckDelay: { description: "Cart Checking Delay (ms)", type: "number", value: 250 },
     cartSkipTimeout: { description: "Cart Skip Timeout (ms)", type: "number", value: 5000 },
@@ -230,7 +230,7 @@ async function resetSaved(skipUnload, fromCart) {
 
     // Setup auto page refresh, not sure if zero value does anything
     // Perform first to reduce chances of not working when tab not focused
-    if(settings.autoReloadInterval.value !== 0) {
+    if(settings.autoReloadInterval.value > 1000) {
         setTimeout(function() {
             window.location.reload();
         }, settings.autoReloadInterval.value);
