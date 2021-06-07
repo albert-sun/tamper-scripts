7b6907b42eb97e420308b656bdcf4bb6ed8fd935
albert-sun <albert.h.sun@gmail.com>
2021-06-06 21:44:02 -0700

    don't trim query params

--- bestbuy-cart/user_interface.js
+++ bestbuy-cart/user_interface.js
@@ -316,7 +316,7 @@ async function designateLogging(loggingWindow, contentDiv) {
     }
 
     // Delimit previous logs, don't show POST data
-    loggingFunction(`== ${location.href.split("?")[0]} ==`); 
+    loggingFunction(`== ${location.href} ==`); 
 
     // Setup copy function (logs as well)
     copyClick.onclick = async function() { 
